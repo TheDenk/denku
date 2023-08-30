a30ea30e8f277ddac616f14e7156289834204a96
Denis <komedian@bk.ru>
2023-08-30 14:08:05 +0300

    Update __init__.py

--- denku/__init__.py
+++ denku/__init__.py
@@ -4,10 +4,11 @@ from .denku import (
     apply_mask_with_gauss, get_color_mask_with_hsv,
     get_mask_for_box, color_mask, draw_box, get_boxes_intersection,
     change_contrast, clear_noise, resize_if_need, make_img_padding,
-    shift_all_colors, split_on_chunks, do_multiprocess, load_json, slerp
+    shift_all_colors, split_on_chunks, do_multiprocess, load_json, slerp,
+    get_linear_value, get_cosine_value, get_ema_value, show_video_in_jupyter
 )
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 __all__ = [
     'download_image', 'show_image', 'show_images',
@@ -15,5 +16,6 @@ __all__ = [
     'get_mask_for_box', 'color_mask', 'draw_box', 'get_boxes_intersection',
     'change_contrast', 'clear_noise', 'resize_if_need', 'make_img_padding',
     'shift_all_colors', 'split_on_chunks', 'do_multiprocess', 'load_json',
-    'slerp',
+    'slerp', 'get_linear_value', 'get_cosine_value', 'get_ema_value', 
+    'show_video_in_jupyter',
 ]
