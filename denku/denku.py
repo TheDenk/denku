4cef519925b2996644440191c960798131dddc41
Denis <komedian@bk.ru>
2024-05-16 14:43:48 +0300

    Update denku.py

--- denku/denku.py
+++ denku/denku.py
@@ -362,3 +362,15 @@ def get_module_memory_gb(module, dtype='fp32'):
         return sum(params) * 4 / 1e9
     else:
         raise NotImplementedError
+
+
+def print_trainable_parameters(model):
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
